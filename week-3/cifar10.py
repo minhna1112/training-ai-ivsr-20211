d6e28a41c4d18057f53d59cdb61ce8ed1da7e4cb
Anh Minh Nguyen <minhngau2804@gmail.com>
2021-12-13 00:02:25 +0700

    Update homework -cifar10.py
    Homework for working with dataset and dataloader

--- week-3/cifar10.py
+++ week-3/cifar10.py
@@ -1,5 +1,4 @@
 import torch
-from  torchvision.datasets import MNIST
 from torch.utils.data import Dataset
 
 import numpy as np
@@ -22,15 +21,112 @@ class Cifar10Dataset(Dataset):
 
     def __getitem__(self, index):
         """
-        TODO: with respect to index, return the coressponding numpy array pf the selected image (using cv2 imread), and the corresponding label (in integer)
+        TODO: with respect to index, return the coressponding selected image path, and the corresponding label (in integer)
         """
         x = ???
         y = ???
         return x, y
 
+class DataLoader:
+    """
+    Dataloader Class
+    Defines an iterable batch-sampler over a given dataset
+    """
+
+    def __init__(self, dataset: Cifar10Dataset, batch_size=16, shuffle=False, drop_last=False):
+        """
+        :param dataset: dataset from which to load the data
+        :param batch_size: how many samples per batch to load
+        :param shuffle: set to True to have the data reshuffled at every epoch
+        :param drop_last: set to True to drop the last incomplete batch,
+            if the dataset size is not divisible by the batch size.
+            If False and the size of dataset is not divisible by the batch
+            size, then the last batch will be smaller.
+        """
+        self.dataset = dataset
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.drop_last = drop_last
+
+    def batch_to_tensor(self, batch: []):
+        ################################################################################################
+        # TODO:                                                                                        #
+        # Define a function that takes a list of batch data and return two tensors x and y             #
+        # x: tensor  of size (batch_size, 3, H, W), y: one-hot encoded tensor of size (batch_size, 10) #
+        # Hint:                                                                                        #
+        #   - Use cv2.imread() to read image using image path in dataset                               #
+        #   - Use torch.nn.functional.one_hot to transform labels inside into binary one-hot tensor    #                                                               # 
+        ################################################################################################
+        
+        return x, y
+
+    def __iter__(self):
+        ########################################################################
+        # TODO:                                                                #
+        # Define an iterable function that samples batches from the dataset.   #
+        # Each batch should be a dict containing numpy arrays of length        #
+        # batch_size (except for the last batch if drop_last=True)             #
+        # Hints:                                                               #
+        #   - np.random.permutation(n) can be used to get a list of all        #
+        #     numbers from 0 to n-1 in a random order                          #
+        #   - To load data efficiently, you should try to load only those      #
+        #     samples from the dataset that are needed for the current batch.  #
+        #     An easy way to do this is to build a generator with the yield    #
+        #     keyword, see https://wiki.python.org/moin/Generators             #
+        #   - For each iteration, yield a batch of data in torch.Tensor form   #
+        #   (Modify batch_to_tensor() method                                   #                             
+        ########################################################################
+        if self.shuffle:
+            index_list = np.random.permutation(len(self.dataset))
+
+        else:
+            index_list = range(len(self.dataset))
+        index_iterator = iter(index_list)  # define indices as iterator
+
+        batch = []
+        for index in index_iterator:  # iterate over indices using the iterator
+            batch.append(self.dataset[index])
+            if len(batch) == self.batch_size:
+                yield ???  # use yield keyword to define a iterable generator
+                batch = []
+
+        #last batch
+        num_complete_batch = int(len(self.dataset) / self.batch_size)
+        if len(self.dataset) % self.batch_size != 0:
+            if self.drop_last is False:
+                batch = [self.dataset[i]
+                         for i in iter(index_list[num_complete_batch * self.batch_size: len(self.dataset)])]
+                yield ???
+
+        pass
+
+        ########################################################################
+        #                           END OF YOUR CODE                           #
+        ########################################################################
+
+    def __len__(self):
+        length = None
+        ########################################################################
+        # TODO:                                                                #
+        # Return the length of the dataloader                                  #
+        # Hint: this is the number of batches you can sample from the dataset  #
+        ########################################################################
+        length = ???
+        if len(self.dataset) % self.batch_size != 0:
+            if not self.drop_last:
+                length = ???
+        pass
+
+        ########################################################################
+        #                           END OF YOUR CODE                           #
+        ########################################################################
+        return length
+
+        
 class Cifar10Classifier(torch.nn.Module):
     def __init__(self):
         super(Cifar10Classifier, self).__init__()
+        self.dataset = None
         self.train_dataset = None
         self.test_dataset = None
         self.dataloader = None
@@ -39,15 +135,20 @@ class Cifar10Classifier(torch.nn.Module):
         self.optimizer = None
 
     def make_data(self):
-        self.train_dataset = Cifar10Dataset(???)
-        self.test_dataset = Cifar10Dataset(???)
+        self.dataset = Cifar10Dataset(???)
+        
+    def split_data(self, split_ratio: float):
+        """
+        - split self.dataset into self.train_dataset and self.test_dataset 
+        - See https://stackoverflow.com/questions/50544730/how-do-i-split-a-custom-dataset-into-training-and-test-datasets
+        """
+        self.train_dataset, self.test_dataset = ???
+        
 
 
 
 
-
-
-
-classifier = HandwrittenDigitClassifier()
+classifier = Cifar10Classifier()
 
 classifier.make_data()
+classifier.split_data()
