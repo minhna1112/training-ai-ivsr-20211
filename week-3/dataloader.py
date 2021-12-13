4ad96dbec1705c8f747a2dbb40825659e7941aad
TuanNV <nguyentuanbarca10@gmail.com>
2021-12-13 22:46:59 +0700

    dataloader

--- week-3/dataloader.py
+++ week-3/dataloader.py
@@ -14,6 +14,7 @@ class Cifar10Loader:
         y = []
         for img, label in batch:
             image = cv2.imread(img)
+            # image = cv2.resize(image, (224, 224))
             x.append(np.array(image) / 255.0)
             y.append(np.array(label))
         x = np.stack(x, axis=0)
@@ -21,7 +22,8 @@ class Cifar10Loader:
         return x, y
     def batch_to_tensor(self, batch: []):
         features, label = self.batch_to_numpy(batch)
-        x = torch.tensor(features, dtype=torch.float32).unsqueeze(1)
+        x = torch.tensor(features, dtype=torch.float32)
+        x = torch.permute(x, (0, 3, 1, 2))
         y = torch.nn.functional.one_hot(torch.LongTensor(label), num_classes=10)
         y = torch.tensor(y, dtype=torch.float32)
         return x, y
@@ -50,4 +52,13 @@ class Cifar10Loader:
                 batch = [self.dataset[i]
                          for i in iter(index_list[num_complete_batch*self.batch_size: len(self.dataset)])]
                 yield self.batch_to_tensor(batch)
+if __name__ == '__main__':
+    path = 'cifar10'
+    data = Cifar10Dataset(path)
+    dtl = Cifar10Loader(data, 32)
 
+    img = cv2.imread('cifar10/bird/0001.png')
+    img = cv2.resize(img, (224, 224))
+    cv2.imshow("Image", img)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
